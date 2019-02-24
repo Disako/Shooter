708432cd7f64a84e90bcac267ad697cba1e88049
Disako <p.b.davies1185@gmail.com>
2019-02-24 18:56:17 +0000

    Making the level more interesting

--- Shooter/Definitions/Levels.lua
+++ Shooter/Definitions/Levels.lua
@@ -42,13 +42,8 @@ level1={
 	{
 		action = "spawn",
 		enemy = "spinner",
-		location = { 560, -20 },
+		location = { 600, -20 },
 		state = "left"
-	},
-	{
-		action = "spawn",
-		enemy = "heavy",
-		location = { 40, -40 }
 	},
 	{
 		action = "wait",
@@ -56,13 +51,363 @@ level1={
 	},
 	{
 		action = "spawn",
+		enemy = "spinner",
+		location = { 40, -20 },
+		state = "right"
+	},
+	{
+		action = "wait",
+		frames = 60
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 560, -20 },
+		state = "left"
+	},
+	{
+		action = "wait",
+		frames = 60
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 80, -20 },
+		state = "right"
+	},
+	{
+		action = "wait",
+		frames = 60
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 520, -20 },
+		state = "left"
+	},
+	{
+		action = "wait",
+		frames = 300
+	},
+	{
+		action = "spawn",
 		enemy = "stomper",
 		location = { 290, -40 }
+	},
+	{
+		action = "wait",
+		frames = 60
 	},
 	{
 		action = "upgrade",
 		upgrader = "upgrader",
 		location = { 110, 480 },
 		state = "left"
-	}
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 310, -20 },
+		state = "left"
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 310, -20 },
+		state = "right"
+	},
+	{
+		action = "wait",
+		frames = 60
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 270, -20 },
+		state = "left"
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 350, -20 },
+		state = "right"
+	},
+	{
+		action = "wait",
+		frames = 60
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 230, -20 },
+		state = "left"
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 390, -20 },
+		state = "right"
+	},
+	{
+		action = "wait",
+		frames = 300
+	},
+	{
+		action = "spawn",
+		enemy = "heavy",
+		location = { 460, -40 }
+	},
+	{
+		action = "wait",
+		frames = 100
+	},
+	{
+		action = "spawn",
+		enemy = "heavy",
+		location = { 460, -40 }
+	},
+	{
+		action = "wait",
+		frames = 100
+	},
+	{
+		action = "spawn",
+		enemy = "heavy",
+		location = { 460, -40 }
+	},
+	{
+		action = "wait",
+		frames = 100
+	},
+	{
+		action = "spawn",
+		enemy = "heavy",
+		location = { 460, -40 }
+	},
+	{
+		action = "wait",
+		frames = 100
+	},
+	{
+		action = "spawn",
+		enemy = "heavy",
+		location = { 460, -40 }
+	},
+	{
+		action = "wait",
+		frames = 100
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 0, -20 },
+		state = "right"
+	},
+	{
+		action = "wait",
+		frames = 60
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 600, -20 },
+		state = "left"
+	},
+	{
+		action = "wait",
+		frames = 60
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 40, -20 },
+		state = "right"
+	},
+	{
+		action = "wait",
+		frames = 60
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 560, -20 },
+		state = "left"
+	},
+	{
+		action = "wait",
+		frames = 60
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 80, -20 },
+		state = "right"
+	},
+	{
+		action = "wait",
+		frames = 60
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 520, -20 },
+		state = "left"
+	},
+	{
+		action = "wait",
+		frames = 100
+	},
+	{
+		action = "upgrade",
+		upgrader = "upgrader",
+		location = { 110, 480 },
+		state = "left"
+	},
+	{
+		action = "wait",
+		frames = 300
+	},
+	{
+		action = "spawn",
+		enemy = "heavy",
+		location = { 140, -40 }
+	},
+	{
+		action = "wait",
+		frames = 100
+	},
+	{
+		action = "spawn",
+		enemy = "heavy",
+		location = { 140, -40 }
+	},
+	{
+		action = "wait",
+		frames = 100
+	},
+	{
+		action = "spawn",
+		enemy = "heavy",
+		location = { 140, -40 }
+	},
+	{
+		action = "wait",
+		frames = 100
+	},
+	{
+		action = "spawn",
+		enemy = "heavy",
+		location = { 140, -40 }
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 0, -20 },
+		state = "right"
+	},
+	{
+		action = "wait",
+		frames = 60
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 600, -20 },
+		state = "left"
+	},
+	{
+		action = "wait",
+		frames = 60
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 40, -20 },
+		state = "right"
+	},
+	{
+		action = "wait",
+		frames = 60
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 560, -20 },
+		state = "left"
+	},
+	{
+		action = "wait",
+		frames = 60
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 80, -20 },
+		state = "right"
+	},
+	{
+		action = "wait",
+		frames = 60
+	},
+	{
+		action = "spawn",
+		enemy = "spinner",
+		location = { 520, -20 },
+		state = "left"
+	},
+	{
+		action = "wait",
+		frames = 100
+	},
+	{
+		action = "spawn",
+		enemy = "stomper",
+		location = { 290, -40 }
+	},
+	{
+		action = "wait",
+		frames = 20
+	},
+	{
+		action = "spawn",
+		enemy = "stomper",
+		location = { 200, -40 }
+	},
+	{
+		action = "spawn",
+		enemy = "stomper",
+		location = { 380, -40 }
+	},
+	{
+		action = "wait",
+		frames = 20
+	},
+	{
+		action = "spawn",
+		enemy = "stomper",
+		location = { 110, -40 }
+	},
+	{
+		action = "spawn",
+		enemy = "stomper",
+		location = { 470, -40 }
+	},
+	{
+		action = "wait",
+		frames = 20
+	},
+	{
+		action = "spawn",
+		enemy = "stomper",
+		location = { 20, -40 }
+	},
+	{
+		action = "spawn",
+		enemy = "stomper",
+		location = { 560, -40 }
+	},
+	{
+		action = "wait",
+		frames = 100
+	},
 }
